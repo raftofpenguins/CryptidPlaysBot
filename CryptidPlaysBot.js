02542431d9f17599a38125827a73f1d2a5c7e3e2
Raft of Penguins <raftofpenguins@gmail.com>
2022-02-19 11:06:06 -0800

    Created initial chatbot

---
+++ CryptidPlaysBot.js
@@ -0,0 +1,57 @@
+// CryptidPlaysBot -- Twitch chat bot for the Cryptid_Plays channel
+// Stream URL: https://www.twitch.tv/cryptid_plays
+// Developed by Raft of Penguins
+
+// The js code below is a modified version of the chatbot_getting_started.js bot posted on the Twitch Developers tutorial
+// Tutorial URL: https://dev.twitch.tv/docs/irc
+
+const tmi = require('tmi.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: <CryptidPlaysBot>,
+    password: <oauth:5t8fl59id6peknqtn5ua6h04sjtyo8>
+  },
+  channels: [
+    <cryptid_plays>
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
